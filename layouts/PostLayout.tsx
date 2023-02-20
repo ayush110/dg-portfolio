bf46ea19bf9b72ea03f83d63febbe8797d1335f5
ayush110 <67121244+ayush110@users.noreply.github.com>
2023-02-19 19:38:13 -0500

    inital-config

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -42,8 +42,8 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
       <article>
         <div className="xl:divide-y xl:divide-gray-200 xl:dark:divide-gray-700">
           <header className="pt-6 xl:pb-6">
-            <div className="space-y-1 text-center">
-              <dl className="space-y-10">
+            <div className=" text-center">
+              <dl className="">
                 <div>
                   <dt className="sr-only">Published on</dt>
                   <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
