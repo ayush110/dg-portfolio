eb3a978ea525bb4b5aa637032b9025723e926dd3
ayush110 <67121244+ayush110@users.noreply.github.com>
2023-12-30 18:37:19 -0500

    footer colors

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,36 +5,36 @@ import { BsSpotify } from 'react-icons/bs'
 export default function Footer() {
   return (
     <footer>
-      <div className="text-xs  ">
+      <div className="text-xs text-zinc-400 ">
         <div className=" text-right">
           <Link
-            className="hover:text-black dark:hover:text-transparent"
+            className=" hover:text-black dark:hover:text-white"
             href={`mailto:${siteMetadata.email}`}
           >
             {siteMetadata.email}
           </Link>
 
           <Link
-            className="flex items-center justify-end text-right hover:text-black dark:hover:text-transparent"
+            className="flex items-center justify-end text-right hover:text-black dark:hover:text-white"
             href={siteMetadata.spotify}
           >
             a side of chai
             <BsSpotify className="ml-1 " />
           </Link>
           <Link
-            className="flex justify-end   hover:text-black dark:hover:text-transparent "
+            className="flex justify-end   hover:text-black dark:hover:text-white "
             href={siteMetadata.linkedin}
           >
             linkedin
           </Link>
           <Link
-            className=" flex justify-end  hover:text-black dark:hover:text-transparent"
+            className=" flex justify-end  hover:text-black dark:hover:text-white"
             href={siteMetadata.twitter}
           >
             twitter
           </Link>
           <Link
-            className="justify-end   hover:text-black dark:hover:text-transparent"
+            className="justify-end   hover:text-black dark:hover:text-white"
             href={siteMetadata.github}
           >
             github
