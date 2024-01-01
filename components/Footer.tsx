4e8919fdca99545b086a9f5076ec10f7fb0b23bb
ayush110 <67121244+ayush110@users.noreply.github.com>
2024-01-01 17:31:59 -0500

    responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,43 +4,39 @@ import { BsSpotify } from 'react-icons/bs'
 
 export default function Footer() {
   return (
-    <footer>
-      <div className="text-xs dark:font-normal  dark:text-zinc-400 ">
-        <div className=" text-right">
-          <Link
-            className=" hover:text-black dark:hover:text-white"
-            href={`mailto:${siteMetadata.email}`}
-          >
-            {siteMetadata.email}
-          </Link>
+    <footer className="mt-8 text-xs dark:font-normal  dark:text-zinc-400 ">
+      <Link
+        className=" flex justify-center hover:text-black  dark:hover:text-white md:justify-end "
+        href={`mailto:${siteMetadata.email}`}
+      >
+        {siteMetadata.email}
+      </Link>
 
-          <Link
-            className="flex items-center text-right hover:text-black dark:hover:text-white sm:justify-end"
-            href={siteMetadata.spotify}
-          >
-            a side of chai
-            <BsSpotify className="ml-1 " />
-          </Link>
-          <Link
-            className="flex hover:text-black  dark:hover:text-white sm:justify-end "
-            href={siteMetadata.linkedin}
-          >
-            linkedin
-          </Link>
-          <Link
-            className=" flex hover:text-black dark:hover:text-white sm:justify-end"
-            href={siteMetadata.twitter}
-          >
-            twitter
-          </Link>
-          <Link
-            className="flex hover:text-black   dark:hover:text-white sm:justify-end"
-            href={siteMetadata.github}
-          >
-            github
-          </Link>
-        </div>
-      </div>
+      <Link
+        className="flex items-center justify-center text-right hover:text-black dark:hover:text-white md:justify-end"
+        href={siteMetadata.spotify}
+      >
+        a side of chai
+        <BsSpotify className="ml-1 " />
+      </Link>
+      <Link
+        className="flex justify-center hover:text-black  dark:hover:text-white md:justify-end "
+        href={siteMetadata.linkedin}
+      >
+        linkedin
+      </Link>
+      <Link
+        className=" flex justify-center hover:text-black dark:hover:text-white md:justify-end"
+        href={siteMetadata.twitter}
+      >
+        twitter
+      </Link>
+      <Link
+        className="flex justify-center hover:text-black dark:hover:text-white md:justify-end"
+        href={siteMetadata.github}
+      >
+        github
+      </Link>
     </footer>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,24 +2,31 @@ import Link from '@/components/Link'
 import { PageSEO } from '@/components/SEO'
 import siteMetadata from '@/data/siteMetadata'
 import Current from '@/components/Current'
+import { useRouter } from 'next/router'
+import Footer from '@/components/Footer'
 
 export default function Home() {
+  const { pathname } = useRouter()
+
+  // Check if the user is on the home page
+  const isHomePage = pathname === '/'
+
   return (
     <>
       <PageSEO title={siteMetadata.author} description={siteMetadata.description} />
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">{HeroSection}</div>
+      <div className="divide-y divide-gray-200 dark:divide-gray-700">{HeroSection(isHomePage)}</div>
     </>
   )
 }
 
-export const HeroSection = (
+export const HeroSection = (isHomePage) => (
   <section className="flex">
     <div className="space-y-2 pt-6 pb-8 md:max-w-3xl md:space-y-4">
-      <h1 className="text-3xl font-bold leading-9 tracking-tight text-gray-900 selection:text-transparent dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-8xl md:leading-14 xl:text-9xl">
+      <h1 className="text-4xl font-bold leading-9 tracking-tight text-gray-900 selection:text-transparent dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-8xl md:leading-14 xl:text-9xl">
         ayush shah
       </h1>
       <br></br>
-      <div className=" shrink text-2xl font-light leading-7  text-gray-900 dark:text-gray-100 md:max-w-xl">
+      <div className="shrink text-2xl font-light leading-7  text-gray-900 dark:text-gray-100 md:max-w-xl">
         <p className="mb-8">
           Welcome! I'm a third year{' '}
           <Link
@@ -44,9 +51,16 @@ export const HeroSection = (
         </p>
       </div>
       <div>
-        <hr className="mt-20 mb-12 w-96 border-black dark:border-white"></hr>
+        <hr className="mt-20 mb-12 border-black dark:border-white md:w-96"></hr>
         <Current />
       </div>
+      {/* Conditionally render the Footer only on the home page */}
+      {isHomePage && (
+        <div className="mt-8 md:hidden">
+          <hr className="mt-20 mb-12  border-black dark:border-white"></hr>
+          <Footer />
+        </div>
+      )}
     </div>
   </section>
 )
